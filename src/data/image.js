2ac1f2e631cd63dcb98dfc6056f92c7adad76284
Lokeshwaran-G1 <lokeshsharp3@gmail.com>
2026-05-20 15:59:07 +0530

    collection page changed by lokesh

--- src/data/image.js
+++ src/data/image.js
@@ -28,6 +28,11 @@ import winter7 from "../img/winter -7.jpg";
 import winter8 from "../img/winter -8.jpg";
 import winter9 from "../img/winter -9.jpg";
 import winter10 from "../img/winter -10.jpg";
+import winter11 from "../img/winter11.webp";
+import winter12 from "../img/winter12.webp";
+import winter13 from "../img/winter13.webp";
+import winter14 from "../img/winter14.avif";
+import winter15 from "../img/winter15.jpg";
 import summer1 from "../img/summer1.webp";
 import summer2 from "../img/summer2.webp";
 import summer3 from "../img/summer3.webp";
@@ -38,6 +43,12 @@ import summer7 from "../img/summer7.webp";
 import summer8 from "../img/summer8.webp";
 import summer9 from "../img/summer9.webp";
 import summer10 from "../img/summer10.webp";
+import summer11 from "../img/summer11.webp";
+import summer12 from "../img/summer12.webp";
+import summer13 from "../img/summer13.jpg";
+import summer14 from "../img/summer14.webp";
+import summer15 from "../img/summer15.webp";
+import summer16 from "../img/summer16.webp";
 
 export const images = {
   modern1, modern2, modern3, modern4, modern5,
@@ -46,8 +57,11 @@ export const images = {
   trade6, trade7, trade8, trade9, trade10,
   winter1, winter2, winter3, winter4, winter5,
   winter6, winter7, winter8, winter9, winter10,
+  winter11,winter12,winter13,winter14,winter15,
   summer1, summer2, summer3, summer4, summer5,
   summer6, summer7, summer8, summer9, summer10,
+  summer11, summer12, summer13, summer14, summer15,
+  summer16,
   // Slider images using existing photos
   Slider1: trade1,
   Slider2: modern1,
